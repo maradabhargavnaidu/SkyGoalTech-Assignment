a13469b2bfdacb42f7e0477309409428f99e0d05
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-03-16 21:23:59 +0530

    completed

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".ham-bars");
-const list = document.querySelector(".none");
-
-hamburger.addEventListener("click", () => {
-  console.log("clicked");
-  list.classList.toggle("none");
-  list.classList.toggle("open");
-});
